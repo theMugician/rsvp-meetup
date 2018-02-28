3816447fcdcb610d8996688f59d2f69eb9a19066
theMugician <gslonina7@gmail.com>
2018-02-27 23:42:50 -0800

    Add Headers

--- popup.js
+++ popup.js
@@ -7,53 +7,43 @@ document.addEventListener('DOMContentLoaded', function() {
     let re = /\d+[^\/]/;
     let counter = 0;
 
+    const events = [];
     const groupName = document.getElementById('group_name').value;
     const amount = parseInt(document.getElementById('amount').value);
     const key = 'key=77564923301b687169247f3028545';
     const endpoint = `https://api.meetup.com/${groupName}/events`;
     const eventsEndpoint = `${endpoint}?${key}`;
 
-    fetch(eventsEndpoint)
+    fetch(eventsEndpoint, {
+      headers: {
+        'origin': 'chrome-extension://*',
+        'content-type': 'application/json',
+        'access-control-allow-origin': '*'
+      },
+    })
       .then(response => response.json())
-      .then(events => console.log(events));
+      .then(data => events = data);
 
     while(counter < amount) {
       let event = events[counter].link;
       let eventId = event.match(re)[0];
-      let eventEndpoint = `${endpoint}/${eventId}/rsvps?${key}&response=yes`;
+      let eventEndpoint = `${endpoint}/${eventId}/rsvps?${key}`;
+      let xhr = new XMLHttpRequest();
 
-      var xhr = new XMLHttpRequest();
       xhr.open('POST', eventEndpoint);
-      xhr.send(null);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.onload = function() {
+        if (xhr.status === 200) {
+            alert('You RSVP\'d YES to event/' + eventId);
+        }
+        else if (xhr.status !== 200) {
+            alert('Request failed.  Returned status of ' + xhr.status);
+        }
+      };
+      xhr.send(encodeURI('response=yes'));
+
       counter++;
     }
-
-    events.forEach((event) => { 
-      let eventId = event.link;
-      eventId = event_id.match(re)[0];
-      let eventEndpoint = `${endpoint}/${eventId}/rsvps?${key}&response=yes`;
-
-    });  
-  });  
-
-  /*
-  checkPageButton.addEventListener('click', function() {
-
-    chrome.tabs.getSelected(null, function(tab) {
-      d = document;
-
-      var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
-    });
-  }, false);
-  */
+  });
   
 }, false);
